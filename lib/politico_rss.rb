6a6548dac30d3bc877b122676fabbbceb114743a
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-05-22 18:42:32 -0600

    writing some documentation

--- lib/politico_rss.rb
+++ lib/politico_rss.rb
@@ -1,5 +1,12 @@
 require "politico_rss/version"
 
 module PoliticoRSS
-  # Your code goes here...
+  # Request information about, and contents of, a given RSS feed.
+  #
+  # @param [String] feed_name The name of the POLITICO RSS feed. Get this from the feed's URL.
+  #
+  # @example PoliticoRSS.feed("politics08")
+  def self.feed(feed_name)
+    puts feed_name
+  end
 end
